6333fff67e1f14154de5a8b72317aee78d01486e
Nixx693 <131728233+Nixx693@users.noreply.github.com>
2023-06-06 19:38:15 -0300

    Add files via upload
    lista de execicios

---
+++ lista2/ex2.py
@@ -0,0 +1,19 @@
+def sequ(n):
+    aux1 = 2
+    aux2 = 3.0
+    soma = 0
+
+    while aux1 <= num:
+        print("aux1, aux2")
+        soma = soma + aux1/aux2
+        aux1 = aux1 + 1
+        aux2 = aux2 + 2
+
+    return soma
+
+num = input('Digite um valor: ')
+while num < 0:
+    num = input('Digite um valor positivo: ')
+
+res = sequ(num)
+print ("res")
---
+++ lista2/ex9.py
@@ -0,0 +1,8 @@
+import math
+num = input('Digite um valor: ')
+if num >= 1 and num <= 3:
+    print("num**2")
+elif num == 4 or num == 9:
+    print("math.sqrt")(num)
+elif num == 6 or num == 7 or num == 8:
+    print("num/9.0")
